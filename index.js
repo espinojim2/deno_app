13eadd4a8e4cf9d0f2acd1ded39c2f0cf22028ec
espinojim2 <espinojim2@gmail.com>
2021-09-05 20:56:41 +0800

    third commit

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router  } from "https://deno.land/x/oak/mod.ts";
+
+
+
+const messages = [];
+
+
+const router = new Router();
+router
+  .get("/", (context) => {
+    context.response.body = "Chat Server!";
+  })
+  .get("/messages", (context) => {
+    context.response.body =messages;
+  })
+  .post("/messages", async(context) => {
+    const  message= await context.request.body().value;   
+      message.push(message);
+    
+  });
 
 const app = new Application();
-
-app.use((ctx) => {
-  ctx.response.body = "Hello world!";
-});
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 addEventListener("fetch", app.fetchEventHandler());
